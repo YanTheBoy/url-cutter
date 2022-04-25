d5b832b5b711651c3e37c88d87bcb5fdeda0bbf1
Ilya Arhipov <igarhipov@sbercloud.ru>
2022-04-26 00:15:49 +0300

    rename func and vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,34 +12,34 @@ var dBEmul map[string]string
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/{id}", returnUrlById)
-	myRouter.HandleFunc("/", shortLongUrl).Methods("POST")
+	myRouter.HandleFunc("/{id}", returnURLByID)
+	myRouter.HandleFunc("/", shortLongURL).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
-func returnUrlById(w http.ResponseWriter, r *http.Request) {
+func returnURLByID(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
-	baseUrl := dBEmul[key]
-	w.Header().Set("Location", baseUrl)
+	baseURL := dBEmul[key]
+	w.Header().Set("Location", baseURL)
 	w.WriteHeader(307)
-	w.Write([]byte(baseUrl))
+	w.Write([]byte(baseURL))
 }
 
-func shortLongUrl(w http.ResponseWriter, r *http.Request) {
+func shortLongURL(w http.ResponseWriter, r *http.Request) {
 	d, err := io.ReadAll(r.Body)
 	// обрабатываем ошибку
 	if err != nil || string([]byte(d)) == "" {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	shortUrl := UrlShortener(string(d))
-	dBEmul[shortUrl] = string(d)
-	w.Write([]byte(shortUrl))
+	shortURL := URLShortener(string(d))
+	dBEmul[shortURL] = string(d)
+	w.Write([]byte(shortURL))
 	w.WriteHeader(201)
 }
 
-func UrlShortener(url string) string {
+func URLShortener(url string) string {
 	shortUrl := []byte(url)
 	urlEnc := base64.StdEncoding.EncodeToString(shortUrl)
 	return urlEnc
