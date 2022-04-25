98affcb5334dd6cd566bc2b6557397339ed7b020
Ilya Arhipov <igarhipov@sbercloud.ru>
2022-04-25 23:13:02 +0300

    add server sets

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"encoding/base64"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+)
+
+var dBEmul map[string]string
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/{id}", returnUrlById)
+	myRouter.HandleFunc("/", shortLongUrl).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func returnUrlById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	baseUrl := dBEmul[key]
+	w.Header().Set("Location", baseUrl)
+	w.WriteHeader(307)
+	w.Write([]byte(baseUrl))
+}
+
+func shortLongUrl(w http.ResponseWriter, r *http.Request) {
+	d, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil || string([]byte(d)) == "" {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	shortUrl := UrlShortener(string(d))
+	dBEmul[shortUrl] = string(d)
+	w.Write([]byte(shortUrl))
+	w.WriteHeader(201)
+}
+
+func UrlShortener(url string) string {
+	shortUrl := []byte(url)
+	urlEnc := base64.StdEncoding.EncodeToString(shortUrl)
+	return urlEnc
+}
+
+func main() {
+	dBEmul = make(map[string]string)
+	handleRequests()
+	http.ListenAndServe(":8080", nil)
+}
