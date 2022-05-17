ed9f5755395c70c4d08a839a42540c9fb0a7cf82
Ilya Arhipov <igarhipov@sbercloud.ru>
2022-05-17 07:46:03 +0300

    add flag parsing

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/iliarkhpv/url-cutter/internal/app"
 	config "github.com/iliarkhpv/url-cutter/internal/cfg"
 	"log"
@@ -12,7 +13,16 @@ func main() {
 		log.Fatal("Server is not running due to: ", err)
 	}
 
+	parseFlags(cfg)
+
 	if err := app.Run(cfg); err != nil {
 		log.Fatal("Some problems with server running", err)
 	}
+}
+
+func parseFlags(cfg *config.Config) {
+	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "SERVER_ADDRESS")
+	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "BASE_URL")
+	flag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "FILE_STORAGE_PATH")
+	flag.Parse()
 }
