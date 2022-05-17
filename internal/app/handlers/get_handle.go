6f9946e34bd40f517e6d76704ecad36f66a8b02c
Ilya Arhipov <igarhipov@sbercloud.ru>
2022-05-17 22:07:12 +0300

    refactoring

--- internal/app/handlers/get_handle.go
+++ internal/app/handlers/get_handle.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -9,10 +10,10 @@ func (h *HTTPHandler) Get() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		URL, err := h.urlStorage.Get(c.Param("id"))
 		if err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
+			return c.String(http.StatusBadRequest, fmt.Errorf("error get URL: %s", err).Error())
 		}
 
-		c.Response().Header().Set(echo.HeaderLocation, URL)
+		c.Response().Header().Set("Location", URL)
 		return c.NoContent(http.StatusTemporaryRedirect)
 	}
 }
